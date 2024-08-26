009984a832209734cd5c9143a1dcb758a755dad3
dimitrimm <dimitrimarinovic@gmail.com>
2024-08-26 16:37:13 -0300

    fix: class sanitaze

--- Assets/Scripts/ProceduralGeneration/MapController.cs
+++ Assets/Scripts/ProceduralGeneration/MapController.cs
@@ -2,22 +2,19 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using UnityEngine.UIElements;
-using static TerrainGenerator;
 
 namespace Assets.Scripts.ProceduralGeneration
 {
     public class MapController : MonoBehaviour
     {
-
         [Header("Tools")]
         [SerializeField] private bool regen = false;
+
         [Header("Tile settings")]
         [SerializeField] private Tilemap tilemap;
         [SerializeField] private TileBase tileMapHighNoiseTile;
         [SerializeField] private TileBase tileMapLowNoiseTile;
         [SerializeField] private bool inverseRender = false;
-
 
         [Header("Map basic Settings")]
         [Range(0, 255)]
@@ -31,26 +28,21 @@ namespace Assets.Scripts.ProceduralGeneration
 
         private bool mapRendered = false;
         private bool pathMapped = false;
-
-
-
-
         private Vector2 spawnPoint;
-        
-        public Vector2 getSpawnPoint() { return spawnPoint; }
 
-        void Start()
+        public Vector2 GetSpawnPoint() => spawnPoint;
+
+        private void Start()
         {
-           TerrainGenerator terrainGenerator = new TerrainGenerator();
-           renderMap(terrainGenerator.buildNoiseMap(seed));
-           tilemap.CompressBounds();
-           tilemap.RefreshAllTiles();
-           mapRendered = true;
+            GenerateAndRenderMap();
+            tilemap.CompressBounds();
+            tilemap.RefreshAllTiles();
+            mapRendered = true;
         }
 
         private void LateUpdate()
         {
-            if (mapRendered & !pathMapped)
+            if (mapRendered && !pathMapped)
             {
                 AstarPath.active.Scan();
                 pathMapped = true;
@@ -59,22 +51,68 @@ namespace Assets.Scripts.ProceduralGeneration
 
         private void Update()
         {
+            HandleMapRegeneration();
+            HandleTileRemoval();
+        }
+
+        private void HandleMapRegeneration()
+        {
             if (regen)
             {
-                TerrainGenerator terrainGenerator = new TerrainGenerator();
-                renderMap(terrainGenerator.buildNoiseMap(seed));
+                GenerateAndRenderMap();
                 tilemap.CompressBounds();
                 tilemap.RefreshAllTiles();
                 regen = false;
                 pathMapped = false;
             }
-            Vector3Int dynVector = GetTilePostitionByClick();
-            if (dynVector!=Vector3Int.zero)
+        }
+
+        private void HandleTileRemoval()
+        {
+            Vector3Int clickedTilePosition = GetTilePositionByClick();
+            if (clickedTilePosition != Vector3Int.zero)
             {
-                RemoveTile(dynVector);
+                RemoveTile(clickedTilePosition);
                 pathMapped = false;
             }
         }
+
+        private void GenerateAndRenderMap()
+        {
+            TerrainGenerator terrainGenerator = new TerrainGenerator();
+            float[,] noiseMap = terrainGenerator.buildNoiseMap(seed);
+            RenderMap(noiseMap);
+        }
+
+        private void RenderMap(float[,] noiseMap)
+        {
+            for (int y = 0; y < noiseMap.GetLength(1); y++)
+            {
+                for (int x = 0; x < noiseMap.GetLength(0); x++)
+                {
+                    TileBase tile = DetermineTile(noiseMap[x, y]);
+                    Vector3Int tilePosition = new Vector3Int(x + (int)offset.x, y + (int)offset.y, 0);
+                    tilemap.SetTile(tilePosition, tile);
+                }
+            }
+        }
+
+        private TileBase DetermineTile(float noiseValue)
+        {
+            bool isHighNoise = inverseRender ? noiseValue < thresHold : noiseValue > thresHold;
+            return isHighNoise ? tileMapHighNoiseTile : tileMapLowNoiseTile;
+        }
+
+        private Vector3Int GetTilePositionByClick()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                return tilemap.WorldToCell(mouseWorldPos);
+            }
+            return Vector3Int.zero;
+        }
+
         public void RemoveTile(Vector3Int tilePosition)
         {
             if (tilemap == null)
@@ -85,7 +123,19 @@ namespace Assets.Scripts.ProceduralGeneration
 
             if (tilemap.HasTile(tilePosition))
             {
+                var tilemapCollider = tilemap.GetComponent<TilemapCollider2D>();
+                if (tilemapCollider != null)
+                {
+                    tilemapCollider.enabled = false;
+                }
+
+                // Modifique os tiles aqui
                 tilemap.SetTile(tilePosition, null);
+                if (tilemapCollider != null)
+                {
+                    tilemapCollider.enabled = true;
+                }
+
                 Debug.Log($"Tile removido na posição {tilePosition}");
             }
             else
@@ -93,70 +143,5 @@ namespace Assets.Scripts.ProceduralGeneration
                 Debug.Log($"Nenhum tile encontrado na posição {tilePosition}");
             }
         }
-
-        private void renderMap(float[,] noiseMap)
-        {
-            for (int y = 0; y < noiseMap.GetLength(1); y++)
-            {
-                for (int x = 0; x < noiseMap.GetLength(0); x++)
-                {
-                    // Define o tile com base no valor do noise
-                    TileBase tile = DetermineTile(noiseMap[x, y]);
-
-                    // Define a posição do tile no Tilemap
-                    Vector3Int tilePosition = new Vector3Int(x + (int)offset.x, y + (int)offset.y, 0);
-
-                    // Define o tile no Tilemap
-                    tilemap.SetTile(tilePosition, tile);
-                }
-            }
-        }
-
-        private TileBase DetermineTile(float noiseValue)
-        {
-            if (inverseRender)
-            {
-                if (noiseValue < thresHold)
-                {
-                    // Retorna o tile específico para valores de noise altos
-                    return tileMapHighNoiseTile;
-                }
-                else
-                {
-                    // Retorna o tile padrão para valores de noise baixos
-                    return tileMapLowNoiseTile;
-                }
-            }
-            else
-            {
-                if (noiseValue > thresHold)
-                {
-                    // Retorna o tile específico para valores de noise altos
-                    return tileMapHighNoiseTile;
-                }
-                else
-                {
-                    // Retorna o tile padrão para valores de noise baixos
-                    return tileMapLowNoiseTile;
-                }
-            }
-        }
-        Vector3Int GetTilePostitionByClick()
-        {
-            // Verifica se o botão esquerdo do mouse foi clicado
-            if (Input.GetMouseButtonDown(0))
-            {
-                // Converte a posição do mouse para as coordenadas do mundo
-                Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-                // Converte a posição do mundo para uma posição no grid do Tilemap
-                Vector3Int tilePosition = tilemap.WorldToCell(mouseWorldPos);
-
-
-                 return tilePosition;
-            }
-            return Vector3Int.zero;
-        }
     }
-
 }
