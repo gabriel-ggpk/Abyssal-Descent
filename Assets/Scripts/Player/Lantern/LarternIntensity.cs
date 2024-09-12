e022f039d4880cc2852b22ec80b59e37fe3d3c92
tiaobraw <tiaobraw@gmail.com>
2024-09-12 16:48:05 -0300

    energy pickup / recharge

--- Assets/Scripts/Player/Lantern/LarternIntensity.cs
+++ Assets/Scripts/Player/Lantern/LarternIntensity.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class LarternIntensity : MonoBehaviour {
 
     [SerializeField] private Light2D lanternLight2D;
     private float initialIntensity;
-    private float duration = 60f;
+    [SerializeField] private float duration = 30f;
+    private const float maxIntensity = 10f;
+    private Coroutine decreaseCoroutine;
 
     // Start is called before the first frame update
     void Start() {
         if (lanternLight2D != null) {
             initialIntensity = lanternLight2D.intensity;
-            StartCoroutine(DecreaseIntensityOverTime());
+            decreaseCoroutine = StartCoroutine(DecreaseIntensityOverTime());
+        } else {
+            Debug.LogError("Lantern Light2D is not assigned!");
         }
     }
 
@@ -25,5 +30,30 @@ public class LarternIntensity : MonoBehaviour {
             yield return null;
         }
         lanternLight2D.intensity = 0;
+        OnIntensityZero();
+    }
+
+    private void OnIntensityZero() {
+        SceneManager.LoadScene("GameOverScene");
+    }
+
+    public float GetIntensity() {
+        return lanternLight2D.intensity;
+    }
+
+    public void Recharge(float rechargeAmount) {
+
+        lanternLight2D.intensity += rechargeAmount;
+
+        if (lanternLight2D.intensity > maxIntensity) {
+            lanternLight2D.intensity = maxIntensity;
+        }
+
+        // Restart the decrease coroutine
+        if (decreaseCoroutine != null) {
+            StopCoroutine(decreaseCoroutine);
+        }
+        initialIntensity = lanternLight2D.intensity;
+        decreaseCoroutine = StartCoroutine(DecreaseIntensityOverTime());
     }
 }
